84c3cca60bb01c3308bf21be25ad90f3f64ea9a9
Rushikesh <rushikhandare2203@gmail.com>
2022-11-26 15:54:23 +0530

    first commit

---
+++ Hamoye_StageA/ha.py
@@ -0,0 +1,28 @@
+import numpy as np 
+array  = ([[94, 89, 63],
+             [93, 92, 48],
+             [92, 94, 56]])
+a = np.array(array)
+print(a[:2,0:])
+
+S = np.array([['him', 'sell'], [90, 28, 43]])
+print(S[0][1][1])
+
+#my_tuppy = (1,2,5,8)
+#print(my_tuppy[2] = 6)
+
+y = [(2, 4), (7, 8), (1, 5, 9)]
+x = y[1][1]
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
